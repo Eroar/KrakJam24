812f49d7a1e632f43a56b2ffa9902fbd23809eed
FilipHand <filif.reka@gmail.com>
2024-01-27 22:07:04 +0100

    added letters

--- game/game.gd
+++ game/game.gd
@@ -1,10 +1,15 @@
 extends Node3D
 
 @export var SCORE = 0
+var letter = preload("res://package/Letter.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in range(50):
+		var new_letter = letter.instantiate()
+		new_letter.position = Vector3(randf_range(-10, 10), 0.1, randf_range(-10, 10))
+		new_letter.rotation = Vector3(0,randf_range(0, 360) ,0)
+		add_child.call_deferred(new_letter)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
