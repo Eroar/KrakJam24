56d31b9acef876ba52ee2eccfae5df39cfe4afed
FilipHand <filif.reka@gmail.com>
2024-01-27 23:44:29 +0100

    EndTransmission added

--- level/Odbior1.gd
+++ level/Odbior1.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 
+var transmission = preload("res://level/EndTransmission.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,4 +13,9 @@ func _process(delta):
 
 
 func _on_interaction_requested(package):
-	print(package)
+	if package != null:
+		var new_trans = transmission.instantiate()
+		add_child.call_deferred(new_trans)
+		package.reparent(new_trans.get_node("PathFollow3D"), false)
+		
+		
