df74f4d94ca1040a8946d4665af5be2d5f006b0a
eroar <48735008+Eroar@users.noreply.github.com>
2024-01-27 01:24:50 +0100

    Export movement variables

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var MOVEMENT_SPEED = 8.0
+@export var ROTATION_SPEED = 5.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -18,15 +18,15 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * MOVEMENT_SPEED
+		velocity.z = direction.z * MOVEMENT_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, MOVEMENT_SPEED)
+		velocity.z = move_toward(velocity.z, 0, MOVEMENT_SPEED)
 
 	if velocity.length() > 0 && velocity.y == 0:
 		var target_direction = transform.origin - velocity
 		var target_rotation = transform.looking_at(target_direction, Vector3.UP).basis
-		transform.basis = transform.basis.slerp(target_rotation, delta * SPEED)
+		transform.basis = transform.basis.slerp(target_rotation, delta * ROTATION_SPEED)
 
 	move_and_slide()
