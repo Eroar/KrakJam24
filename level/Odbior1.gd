4429145f3586e787e526dd51d726b79b9785c12c
eroar <48735008+Eroar@users.noreply.github.com>
2024-01-28 00:55:13 +0100

    Change signal interaction_requested to interaction_start

--- level/Odbior1.gd
+++ level/Odbior1.gd
@@ -7,10 +7,9 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _on_interaction_requested(package):
+func _on__interaction_start(package:RigidBody3D):
 	if package != null:
 		var new_trans = transmission.instantiate()
 		add_child.call_deferred(new_trans)
 		package.reparent(new_trans.get_node("PathFollow3D"), false)
-		
 		
