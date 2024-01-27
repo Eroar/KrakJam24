d498ee64e66c6814b7fe516244a1707abcdf83ae
FilipHand <filif.reka@gmail.com>
2024-01-27 10:32:37 +0100

    odbieranie kod

--- level/odbior.gd
+++ level/odbior.gd
@@ -2,12 +2,13 @@ extends Node3D
 
 
 var has_package: bool = false
-var package = preload("res://package/package.blend")
+@export var package = preload("res://package/package.tscn")
 
 func spawn_node():
 	var new_package = package.instantiate()
-	new_package.position = Vector3(0, 0, 20)
+	new_package.position = $".".position + Vector3(0 ,0, 0)
 	add_child(new_package)
+	has_package = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
