0c5ea24f43d91d054716711b7917f8ce8e3b4b3d
FilipHand <filif.reka@gmail.com>
2024-01-28 11:37:58 +0100

    punkty!

--- level/Odbior1.gd
+++ level/Odbior1.gd
@@ -1,6 +1,10 @@
 extends Node3D
 
 
+enum PCKG_TYPE {YELLOW, RED, BLUE}
+
+@export var TYPE = PCKG_TYPE.YELLOW
+
 var transmission = preload("res://level/EndTransmission.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +13,9 @@ func _ready():
 
 func _on_interaction_start(package:RigidBody3D, interacee:Area3D):
 	if package != null:
-		var new_trans = transmission.instantiate()
-		add_child.call_deferred(new_trans)
-		interacee.unlinkPackage()
-		package.reparent(new_trans.get_node("PathFollow3D"), false)
-		get_parent().SCORE += 5
+		if TYPE == package.TYPE:
+			var new_trans = transmission.instantiate()
+			add_child.call_deferred(new_trans)
+			interacee.unlinkPackage()
+			package.reparent(new_trans.get_node("PathFollow3D"), false)
+			get_parent().SCORE += 5
