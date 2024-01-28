ae31fbe887ffc644d47316001c713a0d8dbfa172
eroar <48735008+Eroar@users.noreply.github.com>
2024-01-28 10:48:27 +0100

    Small improvements

--- game/game.gd
+++ game/game.gd
@@ -3,14 +3,6 @@ extends Node3D
 @export var SCORE:int = 0
 var letter = preload("res://letter/Letter.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	for i in range(60):
-		var new_letter = letter.instantiate()
-		new_letter.position = Vector3(randf_range(-10, 10), 0.1, randf_range(-7, 7))
-		new_letter.rotation = Vector3(0,randf_range(0, 360) ,0)
-		add_child.call_deferred(new_letter)
-
 func _make_it_rain():
 	for i in range(30):
 		var new_letter = letter.instantiate()
