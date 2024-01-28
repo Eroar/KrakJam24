c204c766464919d75d2a8add5ccb9287b7c6223c
FilipHand <filif.reka@gmail.com>
2024-01-28 10:37:36 +0100

    gameplay improvement

--- package/PackagePath.gd
+++ package/PackagePath.gd
@@ -2,9 +2,14 @@ extends Path3D
 
 var t = 0.0
 
+var pkg_list = [preload("res://package/package.tscn"),
+				preload("res://package/packageBlue.blend"),
+				preload("res://package/packageRed.tscn")]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var random_package = pkg_list[randi_range(0, 2)].instantiate()
+	random_package.add_to_group("package")
+	$PathFollow3D.add_child.call_deferred(random_package)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
