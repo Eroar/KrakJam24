38309f5d98a756c0a7f52c0a1007caf6f8563948
eroar <48735008+Eroar@users.noreply.github.com>
2024-01-28 13:11:58 +0100

    Add kierownicy animations

--- kierownicy/kierownikAprobata.gd
+++ kierownicy/kierownikAprobata.gd
@@ -3,5 +3,6 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var anim = get_node("KierownikAprobataMesh/AnimationPlayer")
+	anim.get_animation("Aprobata").loop = true
 	anim.play("Aprobata")
 	pass # Replace with function body.
--- kierownicy/kierownikDysaprobata.gd
+++ kierownicy/kierownikDysaprobata.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# var anim = get_node("KierownikAprobataMesh/AnimationPlayer")
-	# anim.play("Dysaprobata")
+	var anim = get_node("KierownikDysaprobataMesh/AnimationPlayer")
+	anim.get_animation("Dysaprobata").loop = true
+	anim.play("Dysaprobata")
 	pass # Replace with function body.
