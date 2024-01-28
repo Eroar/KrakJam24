9b382082d1851bf322552d73e894d222a8cc283f
eroar <48735008+Eroar@users.noreply.github.com>
2024-01-28 14:17:22 +0100

    Add some audio and bug fixes

--- tables/table.gd
+++ tables/table.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @export var ANIMATION_NAME: String = "Zgniatanie"
 @export var aPlayer: AnimationPlayer = null
+@onready var audioPlayer: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 var type2animName = {0: "Promieniowanie",
 					 1: "Walenie",
@@ -15,17 +16,21 @@ func _on_interaction_start(package, _interactee):
 			return
 		working_package = package
 		aPlayer.play(ANIMATION_NAME)
+		audioPlayer.play()
 		if has_node("DirectionalLight3D"):
 			await get_tree().create_timer(1).timeout
 			$DirectionalLight3D.visible = true
 		return
 
 func _on_interaction_stop(package, _interactee):
+	if package==null:
+		return
 	if type2animName[package.TYPE] != ANIMATION_NAME:
 		return
 	if has_node("DirectionalLight3D"):
 		$DirectionalLight3D.visible = false
 	aPlayer.stop()
+	audioPlayer.stop()
 
 func _on_animation_finished(_anim_name):
 	if type2animName[working_package.TYPE] != ANIMATION_NAME:
