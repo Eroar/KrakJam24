18c7f081d73f45808cd26d7b55836e44e2d74085
FilipHand <filif.reka@gmail.com>
2024-01-28 12:51:51 +0100

    punkciki z animacjami

--- tables/table.gd
+++ tables/table.gd
@@ -3,23 +3,23 @@ extends Node3D
 @export var ANIMATION_NAME: String = "Zgniatanie"
 @export var aPlayer: AnimationPlayer = null
 
+var working_package = null
+
 func _on_interaction_start(package, _interactee):
-	print(package)
 	if package != null:
+		working_package = package
 		aPlayer.play(ANIMATION_NAME)
-		if ANIMATION_NAME == "Zgniatanie":
-			package.PRESSED = true
-		elif ANIMATION_NAME == "Promieniowanie":
-			package.RADIATED = true
-		elif  ANIMATION_NAME == "Walenie":
-			package.HAMMERED = true
-			
-		package.POINT_VALUE += 10
 		return
 
 func _on_interaction_stop(_package, _interactee):
 	aPlayer.stop()
 
 func _on_animation_finished(_anim_name):
-	
+	if ANIMATION_NAME == "Zgniatanie":
+		working_package.PRESSED = true
+	elif ANIMATION_NAME == "Promieniowanie":
+		working_package.RADIATED = true
+	elif  ANIMATION_NAME == "Walenie":
+		working_package.HAMMERED = true
+	working_package.POINT_VALUE += 10
 	print(get_name(), ' skonczyla')
